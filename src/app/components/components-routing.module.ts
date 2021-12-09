2c60a66030e0415bf02f1d22420cc6bfe3d21656
Julian Holfeld <julian@uni-kassel.de>
2021-12-09 18:41:11 +0100

    feat: add routerlink to navbar

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -2,9 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
+import {RulesComponent} from './rules/rules.component';
+import {RanksComponent} from './ranks/ranks.component';
+import {ServicesComponent} from './services/services.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'rules', component: RulesComponent},
+  {path: 'ranks', component: RanksComponent},
 ];
 
 @NgModule({
--- src/app/components/rules/rules.component.spec.ts
+++ src/app/components/rules/rules.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RulesComponent } from './rules.component';
+import {RulesComponent} from './rules.component';
 
 describe('RulesComponent', () => {
   let component: RulesComponent;
@@ -8,9 +8,9 @@ describe('RulesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RulesComponent ]
+      declarations: [RulesComponent],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
