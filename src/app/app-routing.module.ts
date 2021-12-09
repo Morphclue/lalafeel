316bbee8f781018659ff31ebeee969c01ffa6a18
Julian Holfeld <julian@uni-kassel.de>
2021-12-09 19:31:30 +0100

    refactor: components to fc route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: 'components', loadChildren: () => import('./components/components.module').then(p => p.ComponentsModule)},
-  {path: '', pathMatch: 'full', redirectTo: 'components'},
+  {path: 'fc', loadChildren: () => import('./components/components.module').then(p => p.ComponentsModule)},
+  {path: '', pathMatch: 'full', redirectTo: 'fc'},
 ];
 
 @NgModule({
--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -7,10 +7,11 @@ import {RanksComponent} from './ranks/ranks.component';
 import {ServicesComponent} from './services/services.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'services', component: ServicesComponent},
   {path: 'rules', component: RulesComponent},
   {path: 'ranks', component: RanksComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
 ];
 
 @NgModule({
