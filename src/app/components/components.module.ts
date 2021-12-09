3be4527ebc038762a0f4b924e5081c4302aa0037
Julian Holfeld <julian@uni-kassel.de>
2021-12-09 18:02:25 +0100

    feat: generate rules, ranks, services

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,12 +4,18 @@ import {NavbarComponent} from './navbar/navbar.component';
 
 import {HomeComponent} from './home/home.component';
 import {ComponentsRoutingModule} from './components-routing.module';
+import {ServicesComponent} from './services/services.component';
+import {RulesComponent} from './rules/rules.component';
+import {RanksComponent} from './ranks/ranks.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
     HomeComponent,
+    ServicesComponent,
+    RulesComponent,
+    RanksComponent,
   ],
   imports: [
     CommonModule,
