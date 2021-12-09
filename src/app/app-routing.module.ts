d98433ae70964d9969d92665332957f188e641f6
Julian Holfeld <julian@uni-kassel.de>
2021-12-09 15:25:02 +0100

    feat: add paths in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'components', loadChildren: () => import('./components/components.module').then(p => p.ComponentsModule)},
+  {path: '', pathMatch: 'full', redirectTo: 'components'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
