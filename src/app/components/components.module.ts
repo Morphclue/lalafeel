a47688c53550df137083a926cc6f8af7bfa6a727
Julian Holfeld <julian@uni-kassel.de>
2021-12-09 19:32:30 +0100

    refactor: format imports

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {NavbarComponent} from './navbar/navbar.component';
 
+import {NavbarComponent} from './navbar/navbar.component';
 import {HomeComponent} from './home/home.component';
 import {ComponentsRoutingModule} from './components-routing.module';
 import {ServicesComponent} from './services/services.component';
